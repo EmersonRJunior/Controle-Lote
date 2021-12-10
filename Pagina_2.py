33b55d648d3a9ed2659bd14986496c9d0ab391c5
Emerson Ritzmann Junior <emersonritzjunior@gmail.com>
2021-12-10 05:50:09 -0300

    adicionado um scrolled text e labes dinamicos pagina 2

--- Pagina_2.py
+++ Pagina_2.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import scrolledtext
 import glob
 
 
@@ -8,16 +9,16 @@ class Pagina_2:
         self.master = master
         self.list_mes = StringVar()
         self.list_empresa = StringVar()
-        self.sv = StringVar()
-        self.list_sv = ['Romaneio 450', 'dois', 'tres', 'quatro',
-                        'cinco', 'seis', 'sets', 'oito', 'nove', 'dez']
+        self.list_romaneio = ['Romaneio 450', 'dois', 'tres', 'quatro',
+                              'cinco', 'seis', 'sets', 'oito', 'nove', 'Romaneio 460']
+        self.test = []
 
         # Frames principais
-        self.lf_top = LabelFrame(master, text='top')
+        self.lf_top = LabelFrame(master)
         self.lf_top.place(relwidth=1, relheight=1/5)
-        self.lf_mid = LabelFrame(master, text='mid')
+        self.lf_mid = LabelFrame(master)
         self.lf_mid.place(relwidth=1, relheight=3/5, rely=1/5)
-        self.lf_bot = LabelFrame(master, text='bottom')
+        self.lf_bot = LabelFrame(master)
         self.lf_bot.place(relwidth=1, relheight=1/5, rely=4/5)
 
     def top_frames(self):
@@ -59,25 +60,41 @@ class Pagina_2:
     def mid_frames(self):
         # Iniciando os frames de posição
         lf_romaneio = LabelFrame(self.lf_mid, text='Romaneios')
-        lf_romaneio.place(relwidth=2.5/10, relheight=1)
+        lf_romaneio.place(relwidth=3/10, relheight=1)
 
         lf_qntd = LabelFrame(self.lf_mid, text='Qntd')
-        lf_qntd.place(relwidth=2/10, relheight=1, relx=2.5/10)
+        lf_qntd.place(relwidth=2/10, relheight=1, relx=3/10)
 
         lf_preço = LabelFrame(self.lf_mid, text='  R$')
-        lf_preço.place(relwidth=1.5/10, relheight=1, relx=4.5/10)
+        lf_preço.place(relwidth=1.5/10, relheight=1, relx=5/10)
 
         lf_entrada = LabelFrame(self.lf_mid, text='Entrada')
-        lf_entrada.place(relwidth=2/10, relheight=1, relx=6/10)
+        lf_entrada.place(relwidth=2/10, relheight=1, relx=6.5/10)
 
         lf_saida = LabelFrame(self.lf_mid, text='saída')
-        lf_saida.place(relwidth=2/10, relheight=1, relx=8/10)
+        lf_saida.place(relwidth=1.5/10, relheight=1, relx=8.5/10)
 
-        for i in range(10):
+        for i in range(len(self.list_romaneio)):
             romaneio = LabelFrame(lf_romaneio)
             romaneio.place(relwidth=1, relheight=1/10, rely=i/10)
-            lb_romaneio = Label(romaneio, text=f'{self.list_sv[i]}')
+            lb_romaneio = Label(romaneio, text=f'{self.list_romaneio[i]}')
             lb_romaneio.pack(fill='both', expand=1)
+
+    def bot_frames(self):
+        lb = LabelFrame(self.lf_bot, text='Obs:')
+        lb.place(relwidth=0.5, relheight=1)
+        st = scrolledtext.ScrolledText(lb, wrap=WORD)
+        st.place(relwidth=1, relheight=1)
+
+        Button(self.lf_bot, text='teste', command=repeat).pack(side='right')
+        self.test.append(st)
+
+
+def repeat():
+    text = []
+    st = pag_2.test[0].get('1.0', END)
+    text.append(st)
+    print(text)
 
 
 if __name__ == '__main__':
@@ -86,4 +103,6 @@ if __name__ == '__main__':
     pag_2 = Pagina_2(app)
     pag_2.top_frames()
     pag_2.mid_frames()
+    pag_2.bot_frames()
+    repeat()
     app.mainloop()
